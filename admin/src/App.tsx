869c138d7e68cd52d383db909a58198c8aaa86f9
Beth002 <elbethelkami9@gmail.com>
2025-09-09 16:17:14 +0200

    Dark and Light Mode

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeBtn } from "./components/global/theme-btn";
 import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <h1>
         <Button>Click me</Button>
+        <ThemeBtn />
       </h1>
     </div>
   );
--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./components/global/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
