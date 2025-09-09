83cf918357f29b1f4887dabd5ea0a0dc414a95d0
Balekage Gael <gbalekage21@gmail.com>
2025-09-09 12:53:50 +0200

    server init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Api is Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
