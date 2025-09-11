6796218c8199d4f37b5e91ea5071c1d4d8206d44
Balekage Gael <gbalekage21@gmail.com>
2025-09-11 23:05:11 +0200

    vercel error 3 fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,32 @@
-const app = require("./index");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares/error");
+const authRoutes = require("./routes/auth.routes");
+const usersRoutes = require("./routes/users.routes");
+const uplaod = require("express-fileupload");
 
+const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(uplaod());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.get("/", (req, res) => {
+  res.send("Api is warking");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
